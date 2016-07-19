596e17d44b1a79a9d113520509b460627c5fc022
Tom W Hartung <tomwhartung@gmail.com>
2016-07-19 14:35:36 -0600

    EXPERIMENTAL CODE ONLY!!  DO NOT MERGE!!  Added primitive checkpoint displays to index.php .

--- index.php
+++ index.php
@@ -10,46 +10,74 @@
 define('_JEXEC', 1);
 define('DS', DIRECTORY_SEPARATOR);
 
+echo 'checkpoint 1';
+
 if (file_exists(dirname(__FILE__) . '/defines.php')) {
 	include_once dirname(__FILE__) . '/defines.php';
 }
+
+echo 'checkpoint 3';
 
 if (!defined('_JDEFINES')) {
 	define('JPATH_BASE', dirname(__FILE__));
 	require_once JPATH_BASE.'/includes/defines.php';
 }
 
+echo 'checkpoint 5';
+
 require_once JPATH_BASE.'/includes/framework.php';
+
+echo 'checkpoint 7';
 
 // Mark afterLoad in the profiler.
 JDEBUG ? $_PROFILER->mark('afterLoad') : null;
 
+echo 'checkpoint 9';
+
 // Instantiate the application.
 $app = JFactory::getApplication('site');
+
+echo 'checkpoint 11';
 
 // Initialise the application.
 $app->initialise();
 
+echo 'checkpoint 13';
+
 // Mark afterIntialise in the profiler.
 JDEBUG ? $_PROFILER->mark('afterInitialise') : null;
+
+echo 'checkpoint 15';
 
 // Route the application.
 $app->route();
 
+echo 'checkpoint 17';
+
 // Mark afterRoute in the profiler.
 JDEBUG ? $_PROFILER->mark('afterRoute') : null;
+
+echo 'checkpoint 19';
 
 // Dispatch the application.
 $app->dispatch();
 
+echo 'checkpoint 21';
+
 // Mark afterDispatch in the profiler.
 JDEBUG ? $_PROFILER->mark('afterDispatch') : null;
+
+echo 'checkpoint 23';
 
 // Render the application.
 $app->render();
 
+echo 'checkpoint 25';
+
 // Mark afterRender in the profiler.
 JDEBUG ? $_PROFILER->mark('afterRender') : null;
+
+echo 'checkpoint 27';
 
 // Return the response.
 echo $app;
